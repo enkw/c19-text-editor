0ad4b95cd1a1acddfc5e91b7beb5434e85b17546
enkw <iankwolfe@gmail.com>
2024-04-27 00:05:09 -0400

    Cleaned up code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,7 +54,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
